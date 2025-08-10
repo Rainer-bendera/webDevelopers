551b562fecebc0e66fcc1769bb9b0809b52e843f
rainerbendera <rainerelinawinga6@gmail.com>
2025-08-10 13:04:23 +0300

    new changes added

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -1,16 +1,24 @@
 const apiKey = "532b98b2457ee04a6a57afb190028b9a";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unit=metric&q=arusha";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unit=metric&q=";
 
-checkWeather();
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const city = document.querySelector(".city");
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp ) + "˚c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
     
-}
+};
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value)
+});
