d627c23b5a8f2ed6d89f8c71cb49e1cde16495e5
rainerbendera <rainerelinawinga6@gmail.com>
2025-08-26 10:50:35 +0300

    new changes added

--- js/pass.js
+++ js/pass.js
@@ -4,15 +4,33 @@ const requirementList = document.querySelectorAll(".containerList li");
 
 const requirement = [
     {regex: /.{8,}/, index: 0},
-    {regex: /[a-z]/, index: 1},
-    {regex: /[A-Z]/, index: 2},
+    {regex: /[0-9]/, index: 1},
+    {regex: /[a-z]/, index: 2},
     {regex: /[^A-Za-z0-9]/, index: 3},
-    {regex: /[0-9]/, index: 4}, 
+    {regex: /[A-Z]/, index: 4}, 
 ]
 
 inputField.addEventListener("keyup", (e) =>{
     requirement.forEach(item => {
+        const isValid = item.regex.test(e.target.value);
+        const requirementItem = requirementList[item.index];
 
+        if(isValid){
+            requirementItem.firstElementChild.className = "fas fa-check";
+            requirementItem.classList.add("valid");
+        }
+        else
+        {
+            requirementItem.firstElementChild.classList = "far fa-circle";
+            requirementItem.classList.remove("valid");
+        }
     })
 
 });
+
+eye.addEventListener("click", () =>{
+    inputField.type = inputField.type === "password" ? "text" : "password";
+
+
+    eye.classList = `far fa-eye${inputField.type === "password" ? "" : "-slash"}`;
+})
