1f0d69e3431ea2ef91c23515bf7fa6e4a6fa701c
rainerbendera <rainerelinawinga6@gmail.com>
2025-08-27 22:49:08 +0300

    new changes added

--- js/pass.js
+++ js/pass.js
@@ -1,36 +1,35 @@
-const inputField = document.querySelector(".inputField input");
-const eye = document.querySelector(".inputField i");
 const requirementList = document.querySelectorAll(".containerList li");
+const eye = document.querySelector(".inputField i");
+const input = document.querySelector(".inputField input");
+
 
 const requirement = [
-    {regex: /.{8,}/, index: 0},
-    {regex: /[0-9]/, index: 1},
-    {regex: /[a-z]/, index: 2},
-    {regex: /[^A-Za-z0-9]/, index: 3},
-    {regex: /[A-Z]/, index: 4}, 
+    {RegExp: /.{8,}/, index: 0},
+    {RegExp: /[0-9]/, index: 1},
+    {RegExp: /[a-z]/, index: 2},
+    {RegExp: /[^A-Za-z0-9]/, index: 3},
+    {RegExp: /[A-Z]/, index: 4},
 ]
 
-inputField.addEventListener("keyup", (e) =>{
+input.addEventListener("keyup", e => {
     requirement.forEach(item => {
-        const isValid = item.regex.test(e.target.value);
+        const isValid = item.RegExp.test(e.target.value);
         const requirementItem = requirementList[item.index];
 
         if(isValid){
             requirementItem.firstElementChild.className = "fas fa-check";
-            requirementItem.classList.add("valid");
+            requirementItem.classList.add(isValid);
         }
-        else
-        {
-            requirementItem.firstElementChild.classList = "far fa-circle";
-            requirementItem.classList.remove("valid");
+
+        else{
+            requirementItem.firstElementChild.className = "far fa-circle";
+            requirementItem.classList.remove(isValid);
         }
     })
-
-});
+})
 
 eye.addEventListener("click", () =>{
-    inputField.type = inputField.type === "password" ? "text" : "password";
+    input.type = input.type === "password" ? "text" : "password";
 
-
-    eye.classList = `far fa-eye${inputField.type === "password" ? "" : "-slash"}`;
+    eye.className = `far fa-eye${input.type === "password" ? "" : "-slash"}`;
 })
