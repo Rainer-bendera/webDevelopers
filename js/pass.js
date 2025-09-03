d918d8e2f45df79b428216910205daafbc4efa57
rainerbendera <rainerelinawinga6@gmail.com>
2025-09-03 21:50:41 +0300

    new changes added

--- js/pass.js
+++ js/pass.js
@@ -1,18 +1,17 @@
-const requirementList = document.querySelectorAll(".containerList li");
 const eye = document.querySelector(".inputField i");
 const input = document.querySelector(".inputField input");
-
+const requirementList = document.querySelectorAll(".containerList li");
 
 const requirement = [
     {RegExp: /.{8,}/, index: 0},
     {RegExp: /[0-9]/, index: 1},
     {RegExp: /[a-z]/, index: 2},
     {RegExp: /[^A-Za-z0-9]/, index: 3},
-    {RegExp: /[A-Z]/, index: 4},
+    {RegExp: /[A-Z]/, index: 4}
 ]
 
 input.addEventListener("keyup", e => {
-    requirement.forEach(item => {
+    requirement.forEach(item =>{
         const isValid = item.RegExp.test(e.target.value);
         const requirementItem = requirementList[item.index];
 
@@ -20,7 +19,6 @@ input.addEventListener("keyup", e => {
             requirementItem.firstElementChild.className = "fas fa-check";
             requirementItem.classList.add(isValid);
         }
-
         else{
             requirementItem.firstElementChild.className = "far fa-circle";
             requirementItem.classList.remove(isValid);
@@ -28,8 +26,21 @@ input.addEventListener("keyup", e => {
     })
 })
 
-eye.addEventListener("click", () =>{
+eye.addEventListener("click", () => {
     input.type = input.type === "password" ? "text" : "password";
-
     eye.className = `far fa-eye${input.type === "password" ? "" : "-slash"}`;
+})
+
+const input2 = document.querySelector(".wrapper2 input");
+
+input2.addEventListener("input", e =>{
+    localStorage.setItem("cielo", e.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const store = localStorage.getItem("cielo");
+
+    if(store){
+        input2.value = store;
+    }
 })
