56a6ec91ae6cfcf52be7c398efe43609ccd84748
rainerbendera <rainerelinawinga6@gmail.com>
2026-01-09 21:40:04 +0300

    new changes added

--- js/script.js
+++ js/script.js
@@ -16,6 +16,25 @@ if(close){
 };
 
 
+var main = document.getElementById("main");
+var small = document.getElementsByClassName("sm-img");
+
+small[0].onclick = function(){
+    main.src = small[0].src;
+}
+
+small[1].onclick = function(){
+    main.src = small[1].src;
+}
+
+small[2].onclick = function(){
+    main.src = small[2].src;
+}
+
+small[3].onclick = function(){
+    main.src = small[3].src;
+}
+
 
 
 
